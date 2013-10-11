d41326a4dd684bbf1fe3e254cbc583805684da18
Harrisam <samantha.s.harris@googlemail.com>
2013-10-11 10:30:23 +0100

    rails set up

---
+++ PleyApp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PleyApp::Application.config.secret_key_base = '11bf422ebe0eb10c558d077497c10cc9716257f6b65f69f8b28574bb963087f4107c4b808eb8de3c5e3629ae2e6ee9f9012f7af94ff0b3a0773d60d8dcdff2d9'
---
+++ PleyApp/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PleyApp::Application.config.session_store :cookie_store, key: '_PleyApp_session'
